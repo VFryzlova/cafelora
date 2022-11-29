ee7f26a8214ad6e997a201db5fb4ef968601982c
VFryzlova <vendulkaf@gmail.com>
2022-11-29 20:20:40 +0100

    divided into components

---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+export const Header = () => {
+    const header = document.createElement('header')
+    header.innerHTML = `
+    <div class="header__content container">
+        <div class="site-logo"></div>
+
+        <div class="navigation">
+          <button class="nav-btn"></button>
+          <nav class="rollout-nav nav-closed">
+            <a href="#home">domů</a>
+            <a href="#menu">menu</a>
+            <a href="#gallery">galerie</a>
+            <a href="#contact">kontakt</a>
+          </nav>
+        </div>
+
+      </div>
+    `
+    const rolloutNav = header.querySelector('.rollout-nav'),
+    navBtn = header.querySelector('.nav-btn')
+
+    navBtn.addEventListener('click', () => {
+      rolloutNav.classList.toggle('nav-closed')
+    })
+
+    return header
+}
